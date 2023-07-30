152b906284caa2398350f94eef29c1370ffc9077
henry-jacq <henryeditz267@gmail.com>
2023-07-30 18:52:36 +0000

    change: new helper function added

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@ use Dotenv\Dotenv;
 
 include 'vendor/autoload.php';
 include 'config/constants.php';
-include 'config/functions.php';
+include 'config/helper.php';
 
 $dotenv = Dotenv::createImmutable(APP_PATH);
 $dotenv->load();
---
+++ config/helper.php
@@ -0,0 +1,22 @@
+<?php
+
+// Helper functions
+
+/**
+ * Dump and Die
+ */
+function dd($data)
+{
+    echo '<pre>';
+    var_dump($data);
+    echo '</pre>';
+    die();
+}
+
+/**
+ * Return current date and time
+ */
+function now()
+{
+    return date('Y-m-d H:i:s');
+}
