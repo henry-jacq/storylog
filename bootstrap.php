269d7e23c83a510ecc635948253d4f72304c3fb6
henry-jacq <henryeditz267@gmail.com>
2023-07-31 12:39:12 +0000

    change: display error on web page

--- bootstrap.php
+++ bootstrap.php
@@ -6,5 +6,7 @@ include 'vendor/autoload.php';
 include 'config/constants.php';
 include 'config/helper.php';
 
+ini_set('display_errors', true);
+
 $dotenv = Dotenv::createImmutable(APP_PATH);
 $dotenv->load();
