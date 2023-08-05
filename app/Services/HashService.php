bf0a3b6a6e549a2bffcb85d6fa5e4c4cbb3f387f
henry-jacq <henryeditz267@gmail.com>
2023-08-05 11:01:47 +0000

    new: added new hashing functions

--- app/Services/HashService.php
+++ app/Services/HashService.php
@@ -4,7 +4,7 @@ namespace Storylog\Services;
 
 
 class HashService
-{
+{    
     public function hashPassword(string $password): string
     {
         return password_hash($password, PASSWORD_BCRYPT, ['cost' => 12]);
@@ -18,5 +18,15 @@ class HashService
     public function randomHash(int $length): string
     {
         return bin2hex(random_bytes($length));
+    }
+
+    public function md5(string $value)
+    {
+        return md5($value);
+    }
+
+    public function checkMD5(string $value, string $hashedValue)
+    {
+        return md5($value) == $hashedValue;
     }
 }
