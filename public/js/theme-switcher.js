954ed7ed68a3837ac04763eeb7849e1c43006ca2
henry-jacq <henryeditz267@gmail.com>
2023-08-26 13:08:29 +0000

    fix: cannot find selector bug fixed
    The theme changes will occur only if the element placed in DOM

--- public/js/theme-switcher.js
+++ public/js/theme-switcher.js
@@ -7,7 +7,7 @@ let storedTheme = localStorage.getItem("theme-value");
 if (storedTheme == null) {
   // Set dark in theme-value as default
   localStorage.setItem("theme-value", currentTheme);
-} else {
+} else if (document.querySelector("#themeSwitcher > i")) {
   if (storedTheme == "auto" || storedTheme == "light") {
     document.querySelector("html").setAttribute("data-bs-theme", "light");
     document.querySelector("#themeSwitcher > i").classList.remove("bi-moon-stars");
@@ -20,24 +20,26 @@ if (storedTheme == null) {
 }
 
 // Change theme on click
-document.querySelector("#themeSwitcher").addEventListener("click", function () {
-  let themeAttr = "data-bs-theme";
-  let currentTheme = document.querySelector("html").getAttribute(themeAttr);
-
-  if (currentTheme == "light") {
-    document.querySelector("#themeSwitcher").setAttribute("data-bs-title", "Change to dark theme");
-    document.querySelector("#themeSwitcher > i").classList.remove("bi-brightness-high");
-    document.querySelector("#themeSwitcher > i").classList.add("bi-moon-stars");
-    document.querySelector("html").setAttribute(themeAttr, "dark");
-    localStorage.setItem("theme-value", "dark");
-  } else if (currentTheme == "dark") {
-    document.querySelector("#themeSwitcher").setAttribute("data-bs-title", "Change to light theme");
-    document.querySelector("#themeSwitcher > i").classList.remove("bi-moon-stars");
-    document.querySelector("#themeSwitcher > i").classList.add("bi-brightness-high");
-    document.querySelector("html").setAttribute(themeAttr, "light");
-    localStorage.setItem("theme-value", "light");
-  }
-});
+if (document.querySelector("#themeSwitcher > i")) {
+  document.querySelector("#themeSwitcher").addEventListener("click", function () {
+    let themeAttr = "data-bs-theme";
+    let currentTheme = document.querySelector("html").getAttribute(themeAttr);
+  
+    if (currentTheme == "light") {
+      document.querySelector("#themeSwitcher").setAttribute("data-bs-title", "Change to dark theme");
+      document.querySelector("#themeSwitcher > i").classList.remove("bi-brightness-high");
+      document.querySelector("#themeSwitcher > i").classList.add("bi-moon-stars");
+      document.querySelector("html").setAttribute(themeAttr, "dark");
+      localStorage.setItem("theme-value", "dark");
+    } else if (currentTheme == "dark") {
+      document.querySelector("#themeSwitcher").setAttribute("data-bs-title", "Change to light theme");
+      document.querySelector("#themeSwitcher > i").classList.remove("bi-moon-stars");
+      document.querySelector("#themeSwitcher > i").classList.add("bi-brightness-high");
+      document.querySelector("html").setAttribute(themeAttr, "light");
+      localStorage.setItem("theme-value", "light");
+    }
+  });
+}
 
 // To trigger the tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
